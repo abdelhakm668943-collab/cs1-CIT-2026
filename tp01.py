9c2bdefa6daece11f0a8bbe890af1643dada26dc
abdelhak blekahla <abdelhakm668943@gmail.com>
2026-01-08 09:06:55 +0100

    ajout du tp01

---
+++ tp01.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+letters = string.ascii_letters
+
+l = ""
+counter = 0   # initialisation du compteur
+
+while l != "w":
+    l = random.choice(letters)
+    print(f"La lettre choisie est {l}")
+    counter += 1
+
+print(f"Le nombre de tirages effectués est {counter}")
